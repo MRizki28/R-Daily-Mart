ba3f2b04183e03b3551f5cfff41efeada639357c
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-03-07 19:19:10 +0800

    fix

---
+++ src/functions/product.tsx
@@ -0,0 +1,80 @@
+import product1 from "../assets/image/product1.jpg"
+
+
+
+const productFunction= [
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+    {
+        img: product1,
+        category: "Snack & Munchies",
+        title: "Haldiram's Sev Bhujia",
+        price: "Rp 15.000"
+    },
+];
+
+export default productFunction

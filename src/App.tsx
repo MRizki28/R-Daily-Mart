c26f4d69f56167c69c11c0ede7f3bd8a3ad291bb
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-04 13:27:48 +0800

    create admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './base/Home';
+import Home from './FE/base/Home';
+import Admin from './BE/Admin';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/cms/admin' element={<Admin></Admin>}></Route>
     </Routes>
   );
 }
---
+++ src/BE/Admin.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+
+const Admin: React.FC = () => {
+    return (
+        <>
+            <Sidebar></Sidebar>
+        </>
+
+    )
+}
+
+export default Admin;

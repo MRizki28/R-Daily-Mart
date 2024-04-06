1a2aece3aa60d22faf073fe76e9f07ed22b9c4c9
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-06 12:28:58 +0800

    update

--- src/BE/Admin.tsx
+++ src/BE/Admin.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import ProductManagement from "./pages/Product/produtManagement";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Admin: React.FC = () => {
     return (
         <>
+            <Navbar></Navbar>
             <Sidebar></Sidebar>
+            <ProductManagement></ProductManagement>
         </>
 
     )

1073fa51cc9682cb08ab2b2f5792c5618dd68ee0
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-06 12:28:35 +0800

    update tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     'node_modules/flowbite-react/lib/esm/**/*.js',
@@ -10,5 +12,5 @@ module.exports = {
   plugins: [
     require('flowbite/plugin'),
   ],
-}
+})
 

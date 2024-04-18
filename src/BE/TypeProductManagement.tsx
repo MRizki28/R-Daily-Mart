0792b5466795cc468ccc07109edbe1ac289c4a94
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-18 17:10:27 +0800

    update jenis product

---
+++ src/BE/TypeProductManagement.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import ProductManagement from "./pages/Product/produtManagement";
+import 'react-toastify/dist/ReactToastify.css';
+import TypeProductManagement from "./pages/TyperProduct/typeProductManagement";
+
+const TypeProduct: React.FC = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Sidebar></Sidebar>
+            <TypeProductManagement></TypeProductManagement>
+        </>
+
+    )
+}
+
+export default TypeProduct;

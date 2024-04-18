1944b63a39a772effbef17b33ba386dd175a2726
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-18 17:10:31 +0800

    update jenis product

--- src/BE/components/Sidebar.tsx
+++ src/BE/components/Sidebar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { FaProductHunt } from "react-icons/fa";
 import { MdDashboardCustomize } from "react-icons/md";
+import { Link } from "react-router-dom";
+import TypeProductManagement from "../pages/TyperProduct/typeProductManagement";
 
 
 const Sidebar: React.FC = () => {
@@ -20,6 +22,12 @@ const Sidebar: React.FC = () => {
                                 <FaProductHunt className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></FaProductHunt>
                                 <span className="ms-3">Product Management</span>
                             </a>
+                        </li>
+                        <li>
+                            <Link to="/cms/typeproduct" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <FaProductHunt className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></FaProductHunt>
+                                <span className="ms-3">Jenis Product</span>
+                            </Link>
                         </li>
                     </ul>
                 </div>

73bb2710ccd11c55b55bfbe153beb8cf7cb135ed
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-19 12:15:38 +0800

    feat(cart):create cart function

--- src/BE/TypeProductManagement.tsx
+++ src/BE/TypeProductManagement.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import ProductManagement from "./pages/Product/produtManagement";
-import 'react-toastify/dist/ReactToastify.css';
 import TypeProductManagement from "./pages/TyperProduct/typeProductManagement";
 
 const TypeProduct: React.FC = () => {
